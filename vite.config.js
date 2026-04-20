88b3182133e05e5c6091ee584566adc7088a21c9
juwanhong-loreal <127897861+juwanhong-loreal@users.noreply.github.com>
2026-04-20 13:26:30 -0400

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: "/", // must match your repo name exactly
+  base: "/cellbioprint-lot-code-manager/", // must match your repo name exactly
 });
